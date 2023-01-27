80a0f9e82df9bf0d0e032cd8d71ad573ad41e7c1
lossuke <111383037+lossuke@users.noreply.github.com>
2023-01-27 20:29:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,102 @@
+var disableButton = () => {
+    $('.fauxq1').prop('disabled', true);
+};
+$(".vraiq1").click(function(){
+    $(".vraiq1").css("color","green")
+    $(".fauxq1").css("color", "red")
+    $(document).on('click', '.vraiq1', disableButton);
+    $(".vraiq1").disabled = true;
+    $(".txt1").text("Bravo!")
+})
+
+var disableButton = () => {
+    $('.fauxq2').prop('disabled', true);
+};
+
+$(".vraiq2").click(function(){
+    $(".vraiq2").css("color","green")
+    $(".fauxq2").css("color", "red")
+    $(document).on('click', '.vrai', disableButton);
+    $(".vraiq2").disabled = true;
+    $(".txt2").text("Bravo!")
+})
+var disableButton = () => {
+    $('.fauxq3').prop('disabled', true);
+};
+$(".vraiq3").click(function(){
+    $(".vraiq3").css("color","green")
+    $(".fauxq3").css("color", "red")
+    $(document).on('click', '.vrai', disableButton);
+    $(".vraiq3").disabled = true;
+    $(".txt3").text("Bravo!")
+})
+var disableButton = () => {
+    $('.fauxq4').prop('disabled', true);
+};
+$(".vraiq4").click(function(){
+    $(".vraiq4").css("color","green")
+    $(".fauxq4").css("color", "red")
+    $(document).on('click', '.vrai', disableButton);
+    $(".vraiq4").disabled = true;
+    $(".txt4").text("Bravo!")
+})
+var disableButton = () => {
+    $('.fauxq5').prop('disabled', true);
+};
+$(".vraiq5").click(function(){
+    $(".vraiq5").css("color","green")
+    $(".fauxq5").css("color", "red")
+    $(document).on('click', '.vrai', disableButton);
+    $(".vraiq5").disabled = true;
+    $(".txt5").text("Bravo!")
+})
+var disableButton = () => {
+    $('.fauxq6').prop('disabled', true);
+};
+$(".vraiq6").click(function(){
+    $(".vraiq6").css("color","green")
+    $(".fauxq6").css("color", "red")
+    $(document).on('click', '.vrai', disableButton);
+    $(".vraiq6").disabled = true;
+    $(".txt6").text("Bravo!")
+})
+var disableButton = () => {
+    $('.fauxq7').prop('disabled', true);
+};
+$(".vraiq7").click(function(){
+    $(".vraiq7").css("color","green")
+    $(".fauxq7").css("color", "red")
+    $(document).on('click', '.vrai', disableButton);
+    $(".vraiq7").disabled = true;
+    $(".txt7").text("Bravo!")
+})
+var disableButton = () => {
+    $('.fauxq8').prop('disabled', true);
+};
+$(".vraiq8").click(function(){
+    $(".vraiq8").css("color","green")
+    $(".fauxq8").css("color", "red")
+    $(document).on('click', '.vrai', disableButton);
+    $(".vraiq8").disabled = true;
+    $(".txt8").text("Bravo!")
+})
+var disableButton = () => {
+    $('.fauxq9').prop('disabled', true);
+};
+$(".vraiq9").click(function(){
+    $(".vraiq9").css("color","green")
+    $(".fauxq9").css("color", "red")
+    $(document).on('click', '.vrai', disableButton);
+    $(".vraiq9").disabled = true;
+    $(".txt9").text("Bravo!")
+})
+var disableButton = () => {
+    $('.fauxq10').prop('disabled', true);
+};
+$(".vraiq10").click(function(){
+    $(".vraiq10").css("color","green")
+    $(".fauxq10").css("color", "red")
+    $(document).on('click', '.vrai', disableButton);
+    $(".vraiq10").disabled = true;
+    $(".txt10").text("Bravo!")
+})
